ad8496982182025fb21ba5138882003c45375b73
Renato200419 <renato.olivera.c@uni.pe>
2024-06-26 22:38:10 -0500

    New modified

--- src/main/java/uni/isw/model/Cliente.java
+++ src/main/java/uni/isw/model/Cliente.java
@@ -27,9 +27,6 @@ public class Cliente {
 
     @JsonProperty("Cliente_ApeMaterno")
     private String Cliente_ApeMaterno;
-    
-    @JsonProperty("Cliente_Telefono")
-    private String Cliente_Telefono;
 
     @JsonProperty("Cliente_Correo")
     private String Cliente_Correo;
